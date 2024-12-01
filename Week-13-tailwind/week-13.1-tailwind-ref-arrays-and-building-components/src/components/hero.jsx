cfc774dd7c78316ece21f891ade38ae84904ca5f
Chetan <kumarchetan.npr@gmail.com>
2024-12-01 23:06:38 +0530

    Our implementation of multistep form is complete

--- Week-13-tailwind/week-13.1-tailwind-ref-arrays-and-building-components/src/components/hero.jsx
+++ Week-13-tailwind/week-13.1-tailwind-ref-arrays-and-building-components/src/components/hero.jsx
@@ -2,7 +2,10 @@ import { useRef, useState } from 'react'
 import { Button } from './button'
 import { Input } from './input'
 import { HeroSkeleton } from './heroSkeleton'
+import { Otp } from './otp'
+import { z } from 'zod'
 
+const emailSchema = z.string().email()
 
 export function Hero({ disabled }) {
     const [submitDisabled, setSubmitDisabled] = useState(true)
@@ -10,7 +13,17 @@ export function Hero({ disabled }) {
     const [error, setError] = useState('')
     const [title, setTitle] = useState('Verify Your Age')
     const [subtitle, setSubTitle] = useState('Please confirm your birth year. This data will not be stored.')
-    const ref1 = useRef()
+    const [step, setStep] = useState(0)
+    const [email, setEmail] = useState('')
+
+    const isEmailValid = ()=>{
+        try {
+            emailSchema.parse(email)
+            return true
+        } catch (error) {
+            return false
+        }
+    }
 
     function validateBirthYear(value) {
         if (!/^\d{4}$/.test(value)) {
@@ -43,30 +56,42 @@ export function Hero({ disabled }) {
         }
     }
 
-    function handleSubmit() {
+    function handleYearSubmit() {
         if (validateBirthYear(birthYear)) {
             setTitle("Let's Get Started")
             setSubTitle(null)
+            setStep(c=> c+1 )
         }
+    }
+
+    function handleEmailSubmit() {
+        setTitle("Check Your Email For A Code")
+            setSubTitle(`Please enter the verification code sent to your email id ${email}` )
+            if (isEmailValid) {
+                setStep(c=> c+1 )
+            }
     }
 
 
     return < HeroSkeleton title={title} subTitle={subtitle} >
-        <div className="step-1">
+        {step === 0 && <div className="step-1">
             <Input
                 onChange={handleInputChange}
-                reference={ref1}
                 val={birthYear}
                 maxLength={4}
                 type={"number"}
                 placeholder={"Your Birth Year"}
             />
             {error && <p className="error text-red-500 mb-10">{error}</p>}
-            <Button disabled={submitDisabled} onclick={handleSubmit} >  Continue </Button>
-        </div>
-        <div className="step-2">
-            <Input handleInputChange={(e) => setSubmitDiabled(e.target.value.trim() === "")} type={"email"} placeholder={"Email Id"} />
-            <Button disabled={submitDisabled} > Continue </Button>
-        </div>
+            <Button disabled={submitDisabled} onclick={handleYearSubmit} >  Continue </Button>
+        </div> }
+       { step === 1 && <div className="step-2">
+            <Input val={email} onChange={(e) => setEmail(e.target.value)} type={"email"} placeholder={"Email Id"} />
+            <Button onclick={handleEmailSubmit} disabled={!isEmailValid()} > Continue </Button>
+        </div> }
+
+       { step === 2 && <div className="step-3">
+        <Otp otpLength={6} />
+        </div> }
     </HeroSkeleton>
 }
