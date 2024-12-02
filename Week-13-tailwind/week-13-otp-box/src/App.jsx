73b1ee8a7db0b2e41640e7591269e5ccc86cc513
Chetan <kumarchetan.npr@gmail.com>
2024-12-03 04:18:16 +0530

    Initial skeleton of assignment calender app complete

--- Week-13-tailwind/week-13-otp-box/src/App.jsx
+++ Week-13-tailwind/week-13-otp-box/src/App.jsx
@@ -3,83 +3,137 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { Sidebar4 } from './components/sidebars/Sidebar4'
 import { SidebarToggle } from './components/icons/SidebarToggle';
-
-const useMediaQuery = (query) => {
-  const [matches, setMatches] = useState(false);
-
-  useEffect(() => {
-    const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
-    const listener = () => setMatches(media.matches);
-    media.addListener(listener);
-    return () => media.removeListener(listener);
-  }, [matches, query]);
-
-  return matches;
-};
+import { SidebarClass1 } from './components/answers/1-basic-project';
+import { SidebarToggle1 } from './components/icons/SideBarToggle2';
 
 
 function App() {
-
-  const [sidebarOpen, setSidebarOpen] =  useState(true);
-  const isDesktop = useMediaQuery("(min-width: 768px)");
-
-  console.error(isDesktop)
-  
-  useEffect(() => {
-    if (isDesktop == false) {
-      setSidebarOpen(false)
-    } else {
-      setSidebarOpen(true)
-    }
-  }, [isDesktop])
-
-  return (
-    <div className='flex'>
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      <MainContent sidebarOpen={sidebarOpen} />
-    </div>
-  )
+  return <div className='flex '>
+    <Sidebar />
+    <MainContent />
+  </div>
 }
 
-function Sidebar({sidebarOpen, setSidebarOpen}) {
-  if (!sidebarOpen) {
-    return <div className='fixed top-0 left-0'>
-        <div className='cursor-pointer hover:bg-slate-200' onClick={() => {
-          setSidebarOpen(!sidebarOpen)
-        }}>
-          <SidebarToggle />
-        </div>
+
+function Sidebar() {
+  
+  return <aside className='h-screen w-80 bg-red-300 '>
+    <div className="text-black">
+      <SidebarToggle1 />
     </div>
-  }
-    return <div className='w-96 h-screen bg-red-100 fixed top-0 left-0 md:relative'>
-    <div>
-      <div className='cursor-pointer hover:bg-slate-200' onClick={() => {
-        setSidebarOpen(!sidebarOpen)
-      }}>
-        <SidebarToggle />
-      </div>
-    </div>
-  </div>
+  </aside>
 }
 
 function MainContent() {
-  return  <div className='w-full'>
-    <div className='h-72 bg-black hidden md:block'></div>
-    <div className='grid grid-cols-11 gap-8 p-8'>
-      <div className='h-96 rounded-2xl shadow bg-red-200 md:col-span-2 -translate-y-24 shadow-lg  col-span-11 hidden md:block'>
+  
+  return <div className="h-scrren w-full">
+      <div className="w-full h-32 bg-slate-900"></div>
+<div className="grid grid-cols-12 w-full gap-4 p-4">
+        <div className="bg-green-300 rounded-md shadow-md -mt-12 col-span-3 hidden md:block h-[180px]">
+          
+        </div>
+        <div className="bg-blue-300 rounded-md shadow-md md:col-span-5 h-[180px] col-span-12 ">
 
-      </div>
-      <div className='h-96 rounded-2xl shadow bg-green-200 md:col-span-6 shadow-lg col-span-11'>
+        </div>
+        <div className="bg-yellow-300 rounded-md shadow-md md:col-span-4 h-[180px] col-span-12">
 
+        </div>
       </div>
-      <div className='h-96 rounded-2xl shadow bg-yellow-200 md:col-span-3 shadow-lg col-span-11'>
-
-      </div>
-    </div>
-  </div>
+</div>
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const useMediaQuery = (query) => {
+//   const [matches, setMatches] = useState(false);
+
+//   useEffect(() => {
+//     const media = window.matchMedia(query);
+//     if (media.matches !== matches) {
+//       setMatches(media.matches);
+//     }
+//     const listener = () => setMatches(media.matches);
+//     media.addListener(listener);
+//     return () => media.removeListener(listener);
+//   }, [matches, query]);
+
+//   return matches;
+// };
+
+
+// function App() {
+
+//   const [sidebarOpen, setSidebarOpen] =  useState(true);
+//   const isDesktop = useMediaQuery("(min-width: 768px)");
+
+//   console.error(isDesktop)
+  
+//   useEffect(() => {
+//     if (isDesktop == false) {
+//       setSidebarOpen(false)
+//     } else {
+//       setSidebarOpen(true)
+//     }
+//   }, [isDesktop])
+
+//   return (
+//     <div className='flex'>
+//       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+//       <MainContent sidebarOpen={sidebarOpen} />
+//     </div>
+//   )
+// }
+
+// function Sidebar({sidebarOpen, setSidebarOpen}) {
+//   if (!sidebarOpen) {
+//     return <div className='fixed top-0 left-0'>
+//         <div className='cursor-pointer hover:bg-slate-200' onClick={() => {
+//           setSidebarOpen(!sidebarOpen)
+//         }}>
+//           <SidebarToggle />
+//         </div>
+//     </div>
+//   }
+//     return <div className='w-96 h-screen bg-red-100 fixed top-0 left-0 md:relative'>
+//     <div>
+//       <div className='cursor-pointer hover:bg-slate-200' onClick={() => {
+//         setSidebarOpen(!sidebarOpen)
+//       }}>
+//         <SidebarToggle />
+//       </div>
+//     </div>
+//   </div>
+// }
+
+// function MainContent() {
+//   return  <div className='w-full'>
+//     <div className='h-72 bg-black hidden md:block'></div>
+//     <div className='grid grid-cols-11 gap-8 p-8'>
+//       <div className='h-96 rounded-2xl shadow bg-red-200 md:col-span-2 -translate-y-24 shadow-lg  col-span-11 hidden md:block'>
+
+//       </div>
+//       <div className='h-96 rounded-2xl shadow bg-green-200 md:col-span-6 shadow-lg col-span-11'>
+
+//       </div>
+//       <div className='h-96 rounded-2xl shadow bg-yellow-200 md:col-span-3 shadow-lg col-span-11'>
+
+//       </div>
+//     </div>
+//   </div>
+// }
 
 export default App
--- Week-13-tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
+++ Week-13-tailwind/week-13-otp-box/src/components/answers/1-basic-project.jsx
@@ -1,12 +1,21 @@
 // sm, md, lg, xl, 2xl
 
+// export function SidebarClass1() {
+//     return <div className="flex">
+//         <div className="transition-all ease-in-out duration-150 md:w-96  h-screen w-0 ">
+//             Sidebar
+//         </div>
+//         <div className="bg-green-800 h-screen flex-1">
+//             Content
+//         </div>
+//     </div>
+//} 
+
+
 export function SidebarClass1() {
+          
     return <div className="flex">
-        <div className="transition-all ease-in-out duration-150 md:w-96  h-screen w-0 ">
-            Sidebar
-        </div>
-        <div className="bg-green-800 h-screen flex-1">
-            Content
-        </div>
+        <div className="transition-all delay-200 esae-in-out bg-red-200 w-0 h-screen  sm:w-1/4"> Sidebar </div>
+        <div className="bg-green-200 w-full h-screen"> Content </div>
     </div>
 }
