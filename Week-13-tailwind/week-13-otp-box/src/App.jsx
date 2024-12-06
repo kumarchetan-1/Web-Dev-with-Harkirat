0685dc9191facf89ab6e3522f360f5eb441c79ab
Chetan <kumarchetan.npr@gmail.com>
2024-12-06 17:05:57 +0530

    Use of Recoil for state management, showing icons on sidebar collapse, border removed from top bottom of sidebar

--- Week-13-tailwind/week-13-otp-box/src/App.jsx
+++ Week-13-tailwind/week-13-otp-box/src/App.jsx
@@ -2,13 +2,16 @@
 import './App.css'
 import { Sidebar } from './components/sidebars/sidebar';
 import { MainContent } from './components/mainContent/MainContent';
+import { RecoilRoot } from 'recoil';
 
 
 
 function App() {
   return <div className='flex '>
+    <RecoilRoot>
     <div><Sidebar /></div>
     <MainContent />
+    </RecoilRoot>
   </div>
 }
 
---
+++ Week-13-tailwind/week-13-otp-box/src/store/atoms/toggleSidebar.jsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const toggleSidebarAtom = atom({
+    default: false,
+    key: 'toggleSidebarAtom'
+})
