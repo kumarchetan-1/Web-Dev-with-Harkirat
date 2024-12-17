eea37853d7080adf81767b83cf69077e47986856
Chetan <kumarchetan.npr@gmail.com>
2024-12-18 02:58:09 +0530

    Front-end button component complete

--- Week-15-brainly/Brainly-front-end/src/components/ui/Button.tsx
+++ Week-15-brainly/Brainly-front-end/src/components/ui/Button.tsx
@@ -1,20 +1,37 @@
+import { ReactElement } from "react"
 
 
- interface ButtonProps {
+interface ButtonProps {
     variant: "primary" | "secondary"
-    onclick: () => void
+    onclick?: () => void
     size: "sm" | "md" | "lg"
     text: string
-    startIcon: any
-    endIcon: any
-
-
+    startIcon?: ReactElement
+    endIcon?: ReactElement
 }
 
+const btnVariant = {
+    "primary": "bg-purple-700 text-white",
+    "secondary": "bg-purple-200 text-purple-700"
+}
+
+const defaultStyle = 'rounded-md '
+const sizes = {
+    "sm": "px-2 py-1",
+    "md": "px-6 py-2",
+    "lg": "px-4 py-4"
+}
 
 export function Button(props: ButtonProps) {
 
-    return <button>
+    return <button onClick={props.onclick} className={`${btnVariant[props.variant]} ${defaultStyle} ${sizes[props.size]}`} >
+        {(props.startIcon || props.endIcon) ? 
+        <div className='flex gap-1 items-center' >
+            {props.startIcon}
+            <span>{props.text}</span>
+            {props.endIcon}
+        </div> : props.text }
+
 
     </button>
 }
--- Week-15-brainly/Brainly-front-end/tailwind.config.js
+++ Week-15-brainly/Brainly-front-end/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          700: "#4f45e5",
+          200: "#e0e7ff"
+        }
+      }
+    },
   },
   plugins: [],
 }
