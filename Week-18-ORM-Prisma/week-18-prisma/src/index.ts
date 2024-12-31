a61f08335ba3c24ce0cca33c1a68a89919f10041
Chetan <kumarchetan.npr@gmail.com>
2024-12-31 21:55:08 +0530

    expressified the prisma orm based app

--- Week-18-ORM-Prisma/week-18-prisma/src/index.ts
+++ Week-18-ORM-Prisma/week-18-prisma/src/index.ts
@@ -1,14 +1,58 @@
 import { PrismaClient } from "@prisma/client";
+import express from 'express'
 
-const client = new PrismaClient()
+const app = express()
 
- async function createUser(){
-   await client.user.create({
-    // @ts-ignore
-        data: {
-            username: "ChetanKr",
-            
+const client = new PrismaClient({
+    log: ['query']
+})
 
+app.get("/users", async(req, res)=>{
+   const users = await client.user.findMany() 
+   res.json({
+    users
+   })
+})
+
+app.get("/todos/:id", async(req, res)=>{
+    const id = req.params.id
+    const user = await client.user.findFirst({
+        where:{
+            id: parseInt(id)
+        },
+        include:{
+            todos: true
         }
     })
-}
+
+    res.json({
+        user
+    })
+})
+
+
+app.listen(3000, ()=> console.log("App running on port 3000") )
+
+//  async function createUser(){
+//  const user =  await client.user.findFirst({
+//     where: {
+//         id: 1
+//     },
+//     include: {
+//         todos: true
+//     },
+//     // @ts-ignore
+//         // data: {
+//         //     username: "ChetanKr",
+//         //     password: "dsjflasjf",
+//         //     city: "Bijnor",
+//         //     age: 24
+//         // }
+//     })
+
+//     // console.log("Data updated");
+//     console.log(user);
+    
+// }
+
+// createUser()
