fb0e5d6b524089fb992c4eeb485266fd8ae1728c
Chetan <kumarchetan.npr@gmail.com>
2025-01-04 00:22:27 +0530

    Intro-Backend in next js

---
+++ Week-19-NextJs/week-19.1-next-app-2/app/api/v1/user/details/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+
+
+export function GET() {
+    
+return NextResponse.json({
+    name: "Chetan Kumar",
+    email: "kumarchetan.npr@gmail.com"
+})
+}
+
+export function POST() {
+    
+return NextResponse.json({
+    name: "Chetan Kumar",
+    email: "kumarchetan.npr@gmail.com"
+})
+}
+
+export function PUT() {
+    
+return NextResponse.json({
+    name: "Chetan Kumar",
+    email: "kumarchetan.npr@gmail.com"
+})
+}
+
+export function DELETE() {
+    
+return NextResponse.json({
+    name: "Chetan Kumar",
+    email: "kumarchetan.npr@gmail.com"
+})
+}
