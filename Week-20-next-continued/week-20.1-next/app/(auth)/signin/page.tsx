0ae4acc70b97f98c6a938dd7a52f4883832a9903
Chetan <kumarchetan.npr@gmail.com>
2025-01-06 02:41:57 +0530

    Week-20.1 init, route groups

---
+++ Week-20-next-continued/week-20.1-next/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Signin() {
+    
+    return <div>
+        <input type="text" name="usename" />
+        <input type="password" name="password" />
+        <button>Sign in</button>
+    </div>
+}
---
+++ Week-20-next-continued/week-20.1-next/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Signup() {
+    
+    return <div>
+        <input type="text" name="usename" />
+        <input type="password" name="password" />
+        <button>Sign up</button>
+    </div>
+}
