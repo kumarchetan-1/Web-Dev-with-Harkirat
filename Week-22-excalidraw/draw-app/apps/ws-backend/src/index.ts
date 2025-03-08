65c59af28231f7ef01112c880e337cb3ce7a6b8b
Chetan <kumarchetan.npr@gmail.com>
2025-03-09 00:37:04 +0530

    created turbo.json for backend outputs in week-22-excalidraw

--- Week-22-excalidraw/draw-app/apps/ws-backend/src/index.ts
+++ Week-22-excalidraw/draw-app/apps/ws-backend/src/index.ts
@@ -3,8 +3,16 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 })
 
 wss.on("connection", function connection(ws) {
-    
+    console.log("New connection established");
+
+    ws.send("Hello from WebSocket server")
     ws.on("message", function message(data) {
         ws.send("pong")
     })
+
+    ws.on('close', ()=>{
+        console.log('Client disconnected');
+    })
 })
+
+console.log('WebSocket server running on ws://localhost:8080');
