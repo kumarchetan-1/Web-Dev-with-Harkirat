91023f633ef081beb24841fe2afe49c1caa00d6b
Chetan <kumarchetan.npr@gmail.com>
2025-03-13 17:08:46 +0530

    changed request types in http-backend, imported the prisma client correctly in db, used the backend schema in http-backend

--- Week-22-excalidraw/draw-app/apps/http-backend/src/index.ts
+++ Week-22-excalidraw/draw-app/apps/http-backend/src/index.ts
@@ -2,24 +2,40 @@ import express from "express"
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config"
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from '@repo/common/types'
+import { prismaClient } from "@repo/db/client"
 
 const app = express()
 app.use(express.json())
 
-app.get("/sigup", (req, res)=>{
-    const data = CreateUserSchema.safeParse(req.body) 
-   if(!data.success){
+app.post("/sigup", (req, res)=>{
+    const paraseData = CreateUserSchema.safeParse(req.body) 
+   if(!paraseData.success){
     res.json({
      message: "Incorrect inputs"
     })
     return 
    } 
-    res.json({
+
+try {
+    prismaClient.user.create({
+        data:{
+          email: paraseData.data?.username,
+        password: paraseData.data.password,
+        name: paraseData.data.name
+        }
+     })
+     res.json({
         userId: 1234
     })
+} catch (error) {
+    res.status(411).json({
+        message: "User name already exists"
+    })
+}
+
 })
 
-app.get("/sigin", (req, res)=>{
+app.post("/sigin", (req, res)=>{
     const data = SigninSchema.safeParse(req.body) 
     if(!data.success){
      res.json({
@@ -36,7 +52,7 @@ app.get("/sigin", (req, res)=>{
     })
 })
 
-app.get("/create-room", (req, res)=>{
+app.post("/create-room", (req, res)=>{
     const data = CreateRoomSchema.safeParse(req.body) 
    if(!data.success){
     res.json({
